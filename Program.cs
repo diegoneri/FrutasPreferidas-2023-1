4a1dce2c0032fba90e3642af5f7fe17934b9eb60
ADOLPHOBEREZIN\informatica <joao.rossi14@etec.sp.gov.br>
2023-03-03 20:43:35 -0300

    Finaliza exercício FrutasPreferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Minhas frutas preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1. Morango");
+Console.WriteLine("2. Mamão");
+Console.WriteLine("3. Abacaxi");
+Console.WriteLine("4. Tomate");
+Console.WriteLine("5. Uva");
