f6415e17fed59fa0219c4cfda1d475d3ee6247ff
Diego Neri (ETEC) <diego.neri@etec.sp.gov.br>
2023-03-03 21:35:49 -0300

    Remove comentário

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("--- Minhas frutas preferidas ---");
+﻿Console.WriteLine("--- Minhas frutas preferidas ---");
 Console.WriteLine();
 Console.WriteLine("1. Morango");
 Console.WriteLine("2. Mamão");
